28c6d7a2e30690826f091afb600939a01f330c5e
Pedro Manoel <pedroszdev@outlook.com>
2025-06-16 12:13:59 -0300

    verificador pronto

--- VerificadorDeCPF.py
+++ VerificadorDeCPF.py
@@ -1,19 +1,22 @@
-cpf=input('Digite seu CPF: ').split('-')
-cpf_semponto=cpf[0].split('.')
-multi=10
-multi_numeros=[]
-primeiro_digito_cpf = int(cpf[1][0])
-for partecpf in cpf_semponto:
-    for numero in partecpf:
-        numeroint = int(numero)
-        result=multi*numeroint
-        multi_numeros.append(result)
-        multi-=1
-soma=sum(multi_numeros)
-multi10=soma*10
-resto_div=multi10%11
-primeiro_digito_cpf_da_verificação= 0 if resto_div > 9 else resto_div
-if primeiro_digito_cpf_da_verificação == primeiro_digito_cpf:
-    print('Seu CPF esta correto')
+cpf='009811700990'
+nove_digitos=cpf[:9]
+dez_digitos=cpf[:10]
+ultimo_dois_digitos=cpf[9:]
+contador_regressivo_1=10
+contador_regressivo_2=11
+resultado_digito_1=0
+resultado_digito_2=0
+for digito in nove_digitos:
+    resultado_digito_1+= int(digito)*contador_regressivo_1
+    contador_regressivo_1-=1
+digito_1=(resultado_digito_1*10) %11
+digito_1= 0 if digito_1 > 9 else digito_1
+for digito in dez_digitos:
+    resultado_digito_2 += int(digito)*contador_regressivo_2
+    contador_regressivo_2-=1
+digito_2=(resultado_digito_2*10) %11
+digito_2=0 if digito_2 > 9 else digito_2
+if digito_1 == int(ultimo_dois_digitos[0]) and digito_2 == int(ultimo_dois_digitos[1]):
+    print('Seu CPF está correto')
 else:
-    print('Seu CPF esta incorreto')
+    print('Seu CPF não está correto')
